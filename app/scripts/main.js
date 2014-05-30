3652eaf4a113465f1496ec28a1643b0d392c4ddc
Josh Date <date0010@me.com>
2014-05-30 00:47:44 -0400

    added proof of concept functions and styling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,6 +8,33 @@
 // 	meta:"", 
 // 	appID: static tag for identification
 // }
+	// else {
+	// 	throw new Error('Return does not contain the properties user, message and/or time.')
+	// }
+
+
+var messageTemplate = _.template($('.message-reply-template').text());
+
+function messageRendering (data) {
+		data.forEach(function (info) {
+			if(info.user && info.time && info.message) {
+				// add in regex for img set
+				info.time = (moment(parseInt(info.time)).fromNow());
+				var messageContent = messageTemplate(info);
+				$('.message-list').prepend(messageContent);
+			}
+		})
+		$(".messages-wrapper").scrollTop($('.messages-container').height())
+}
+
+
+
+$.getJSON('http://tiny-pizza-server.herokuapp.com/collections/chat-messages').done(function (reply) {
+	messageRendering(reply);
+	
+});
+
+
 
 
 function MessageForMe () {
@@ -15,6 +42,14 @@ function MessageForMe () {
 } 
 
 function MessageForYou () {
-	
+
 }
 
+
+$('button').click(function () {
+	var message = $('input').val();
+	var rightNow = moment().format('X')*1000;
+	var name = 'JD';
+	$.post('http://tiny-pizza-server.herokuapp.com/collections/chat-messages', {'user': name , 'time': rightNow, 'message': message});
+
+})
