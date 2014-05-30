fcc2761e7a35daa2f99a9cab2a7ab9caa24097e8
Josh Date <date0010@me.com>
2014-05-30 02:11:14 -0400

    more styling and more fun scripts for pulling db info

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -24,15 +24,17 @@ function messageRendering (data) {
 				$('.message-list').prepend(messageContent);
 			}
 		})
-		$(".messages-wrapper").scrollTop($('.messages-container').height())
 }
 
 
+// setInterval(function () {
 
-$.getJSON('http://tiny-pizza-server.herokuapp.com/collections/chat-messages').done(function (reply) {
-	messageRendering(reply);
-	
-});
+	$.getJSON('http://tiny-pizza-server.herokuapp.com/collections/chat-messages').done(function (reply) {
+		messageRendering(reply);
+		$(".messages-wrapper").scrollTop($('.messages-container').height())
+		
+	});
+// }, 1000);
 
 
 
@@ -53,3 +55,16 @@ $('button').click(function () {
 	$.post('http://tiny-pizza-server.herokuapp.com/collections/chat-messages', {'user': name , 'time': rightNow, 'message': message});
 
 })
+
+
+
+// $.getJSON('http://tiny-pizza-server.herokuapp.com/collections/chat-messages').done(function (data) {
+//   data.forEach(function (object) {
+//     var id = object._id;
+//      $.ajax({
+//      	url:'http://tiny-pizza-server.herokuapp.com/collections/chat-messages/'+id,
+//      	type:'PUT', 
+//      	data: {'name': 'Dread Pirate Roberts', 'message': "<img src='http://blog.boreal-kiss.net/wp/wp-content/uploads/2012/09/large.gif'/>", 'isOfTheNight': true}
+//      });
+// })})
+
